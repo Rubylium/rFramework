0f35330ff2d8b2cd7e5cee322f3087df95a1d5cc
KXWXIY <44126911+KXWXIY@users.noreply.github.com>
2020-05-04 11:15:45 +0200

    Add files via upload

---
+++ language/french.lua
@@ -0,0 +1,29 @@
+Locales['french'] = {
+	-- Welcome
+	["last_pos"] = "Vous avez été téléporté à votre dernière position",
+	["welcome"] = "Bienvenue, nous préparons votre personnage...",
+	["welcome_back"] = "Chargement de votre personnage en cours...",
+	-- Logs
+	["new_user"] = "Nouvel utilisateur: ",
+	["user"] = "Utilisateur: ",
+	-- Position
+	["position_save"] = "Position sauvegardée",
+	["new_spawn_user"] = "Apparition de l'utilisateur: ",
+	["user_reconnect"] = "Reconnexion de l'utilistaeur: ",
+	["user_reconnect2"] = "Téléportation aux dernières positions effectuée",
+	-- Version
+	["startup"] = "^1Votre version n'est pas à jour, visitez https://github.com/Rubylium/rFramework pour la mettre à jour^2",
+	["startup2"] = "^1Version actuelle:^7 ",
+	["startup3"] = "^1Version mise à jour:^7 ",	
+	["startup4"] = "^1Votre version semble être supérieure à la version actuelle^2",
+	["startup5"] = "^2Tout fonctionne correctement, le framework est démarré^2",
+	["settings"] = "^2Langue:^7 ",
+	["settings2"] = "^2Logs:^7 ",
+    -- Notifications
+    ["add_money_wallet"] = "Argent liquide ajoutée",
+	["remove_money_wallet"] = "Argent liquide retirée",
+	["add_bank_money"] = "Argent banque ajoutée",
+    ["bank_money_removed"] = "Argent banque retirée",
+	["add_diry_money"] = "Argent sale ajoutée",
+	["remove_dirty_money"] = "Argent sale retirée",
+}
