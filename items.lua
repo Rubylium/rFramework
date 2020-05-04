a15c095fb3c61f8f49d1389ae587266d31d7f821
KXWXIY <44126911+KXWXIY@users.noreply.github.com>
2020-05-04 10:59:35 +0200

    label system for item #02

--- items.lua
+++ items.lua
@@ -1,6 +1,7 @@
 
 -- All items with there weight
 items = {
-    {name="Bread", weight=0.3},
-    {name="Apple", weight=0.2},
+    {label = "Pain", name = "Pain", weight = 0.3},
+    {label = "Pomme", name = "Pomme", weight = 0.2},
+    {label = "Eau", name = "Eau", weight = 0.3},
 }
