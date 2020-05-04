452a25a80a360c73cf101fbee9fdf2e5d06c3d62
KXWXIY <44126911+KXWXIY@users.noreply.github.com>
2020-05-04 10:59:59 +0200

    label system for item #02

--- server/player/inventory/srv_inv_events.lua
+++ server/player/inventory/srv_inv_events.lua
@@ -3,45 +3,52 @@
 RegisterNetEvent("rF:GetPlayerInventory")
 AddEventHandler("rF:GetPlayerInventory", function()
     local inv = GetInventoryFromCache(source)
-    local weight = GetInvWeight(inv)
-    TriggerClientEvent("rF:SendPlayerInventory", source, inv, weight)
+    TriggerClientEvent("rF:SendPlayerInventory", source, inv)
 end)
 
 
 RegisterNetEvent("rF:GetOtherPlayerInventory")
 AddEventHandler("rF:GetOtherPlayerInventory", function(target)
     local inv = GetInventoryFromCache(target)
-    local weight = GetInvWeight(inv)
-    TriggerClientEvent("rF:SendPlayerInventory", source, inv, weight)
+    TriggerClientEvent("rF:SendPlayerInventory", source, inv)
 end)
 
 
 RegisterNetEvent("rF:GiveItem")
-AddEventHandler("rF:GiveItem", function(token, item, count)
+AddEventHandler("rF:GiveItem", function(token, item, count, label)
     if CheckToken(token, source) then
-        AddItemToPlayerInv(source, item, count)
+        AddItemToPlayerInv(source, item, count, label)
     end
 end)
 
+
 RegisterNetEvent("rF:GiveItemToPlayer")
-AddEventHandler("rF:GiveItemToPlayer", function(token, target, item, count)
+AddEventHandler("rF:GiveItemToPlayer", function(token, target, item, count, label)
     if CheckToken(token, source) then
-        AddItemToPlayerInv(target, item, count)
+        AddItemToPlayerInv(target, item, count, label)
     end
 end)
 
 
 RegisterNetEvent("rF:RemoveItem")
-AddEventHandler("rF:RemoveItem", function(token, item, count)
+AddEventHandler("rF:RemoveItem", function(token, item, count, label)
     if CheckToken(token, source) then
-        RemoveItemFromPlayerInv(source, item, count)
+        RemoveItemFromPlayerInv(source, item, count, label)
     end
 end)
 
 
 RegisterNetEvent("rF:RemoveItemToPlayer")
-AddEventHandler("rF:RemoveItemToPlayer", function(token, target, item, count)
+AddEventHandler("rF:RemoveItemToPlayer", function(token, target, item, count, label)
     if CheckToken(token, source) then
-        RemoveItemFromPlayerInv(target, item, count)
+        RemoveItemFromPlayerInv(target, item, count, label)
+    end
+end)
+
+
+RegisterNetEvent("rF:RenameItem")
+AddEventHandler("rF:RenameItem", function(token, item, count, label)
+    if CheckToken(token, source) then
+        RenameItem(source, item, count, label)
     end
 end)
